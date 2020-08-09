dd4faa02af718d9a5eaa82c2b321e4ebc5a770f9
SusuTawar <realsusutawar@gmail.com>
2020-08-09 16:21:43 +0700

    fixing data entry

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
-const express = require("express");
+const helmet = require("helmet");
 const morgan = require("morgan");
+const express = require("express");
 const rawBody = require("raw-body");
 const socketio = require("socket.io");
 const querystring = require("querystring");
@@ -19,36 +20,42 @@ const server = createServer(app);
 const io = socketio(server);
 
 app.use(morgan("tiny"));
+app.use(helmet());
 
 scheduleJob("0 * * *", () => {
   readdirSync(join(__dirname, "./channels")).forEach((file) => {
     const g = require(`./channels/${file}`);
     const group = g.split(".")[0];
-    g.forEach(async (channel) => {
+    g.forEach((channel) => {
       const name =
         `${group}_` +
-        channel.name.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match) {
-          return +match === 0 ? "" : match.toUpperCase();
-        });
-      await subscribe(name, channel.id);
+        channel.name
+          .toLowerCase()
+          .replace(/\b\w/g, (match) => match.toUpperCase())
+          .replace(/\s+/g, "");
+      subscribe(name, channel.id).then();
     });
   });
 });
 
 async function subscribe(name, chid) {
-  await axios({
-    url: "http://pubsubhubbub.appspot.com/",
-    method: "POST",
-    headers: { "content-type": "application/x-www-form-urlencoded" },
-    data: querystring.stringify({
-      "hub.mode": "subscribe",
-      "hub.callback": `https://hollow-notifier.glitch.me/psh/yt/${name}`,
-      "hub.topic": `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${chid}`,
-      "hub.lease_seconds": `${24 * 60 * 60}`,
-      "hub.secret": process.env.wesub_secret,
-    }),
-  });
-  logger.info(`Subscribe to [${name}] at [${chid}]`);
+  try {
+    await axios({
+      url: "http://pubsubhubbub.appspot.com/",
+      method: "POST",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: querystring.stringify({
+        "hub.mode": "subscribe",
+        "hub.callback": `https://hollow-notifier.glitch.me/psh/yt/${name}`,
+        "hub.topic": `https://www.youtube.com/xml/feeds/videos.xml?channel_id=${chid}`,
+        "hub.lease_seconds": `${24 * 60 * 60}`,
+        "hub.secret": process.env.wesub_secret,
+      }),
+    });
+    logger.info(`Subscribe to [${name}] at [${chid}]`);
+  } catch (error) {
+    logger.error(error);
+  }
 }
 
 app.get("/psh/yt/:id", async (req, res) => {
@@ -64,41 +71,47 @@ app.get("/psh/yt/:id", async (req, res) => {
   }
 });
 
-app.post("/psh/yt/:id", async (req, res) => {
-  const r = await parseStringPromise(await checkSign(req));
+app.post("/psh/yt/:id", checkSign, (req, res) => {
+  if (!req.body.verified) return res.status(403).send("");
   const entry =
-    "at:deleted-entry" in r ? r.feed["at:deleted-entry"][0] : r.feed.entry[0];
+    "at:deleted-entry" in req.body.feed
+      ? req.body.feed["at:deleted-entry"][0]
+      : req.body.feed.entry[0];
   const [action, response] = [
-    "at:deleted-entry" in r ? "vid-remove" : "vid-update",
+    "at:deleted-entry" in req.body.feed ? "vid-remove" : "vid-update",
     {
       group: req.params.id.split("_")[0],
       channel: req.params.id.split("_")[1].replace(/([A-Z])/, " $1"),
-      title: entry.title || "Not Defined",
+			title: entry.title? entry.title[0] : "Not Defined",
       link:
         "yt:videoid" in entry
           ? entry["yt:videoid"][0]
-          : entry.link[0].$.href.substr(32),
+          : entry.link[0].$.href.split("watch?v=")[1],
     },
   ];
+	console.log(response);
   logger.info(`[${action}] [${req.params.id}] [${response.link}]`);
   io.to("/").emit(action, response);
   io.to(response.group).emit(action, response);
   res.status(200).send("");
 });
 
-async function checkSign(req) {
-  const xhs = req.headers["X-Hub-Signature"];
-  if (!xhs) return true;
-  const method = xhs.split("=")[0];
-  const signature = xhs.split("=")[1];
-  const raw = await rawBody(req);
+async function checkSign(req, res, next) {
+  try {
+    const xhs = req.headers["x-hub-signature"] || req.headers["X-Hub-Signature"];
+    if (!xhs) return res.status(403).send('');
+    const method = xhs.split("=")[0];
+    const signature = xhs.split("=")[1];
+    const raw = await rawBody(req);
 
-  const csign = createHmac(method, process.env.wesub_secret);
-  csign.update(raw);
-  return {
-    verify: signature == `sha1=${csign.digest("hex")}`,
-    body: raw,
-  };
+    const csign = createHmac(method, process.env.wesub_secret);
+    csign.update(raw);
+    req.body = await parseStringPromise(raw);
+    req.body.verified = signature == `${method}=${csign.digest("hex")}`;
+    next();
+  } catch (error) {
+    next(error);
+  }
 }
 
 app.listen(3000, () => {
@@ -106,4 +119,3 @@ app.listen(3000, () => {
   io.listen(4000);
   logger.info("Running socket.io");
 });
-
